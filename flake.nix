569c902876b3f61dd85c29dfaf670a4ba134fbd8
Radosaw Kotkiewicz <radoslaw.kotkiewicz@gmail.com>
2026-02-03 21:56:30 +0100

    add simple raspberry 3 config

--- flake.nix
+++ flake.nix
@@ -1,15 +1,22 @@
 {
-  description = "A very basic flake";
+  description = "My NixOs Home Assistant configuration for Raspberry Pi 3 Model B";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    hardware.url = "github:nixos/nixos-hardware";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, hardware, ... }@inputs: {
+    nixosConfigurations = {
+      "malina" = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+        modules = [
+          ./configuration.nix
+          hardware.nixosModules.raspberry-pi-3
+        ];
+      };
+    };
   };
 }
