98646378f3d54b67c71b85b3b50419b1f186f582
Radosaw Kotkiewicz <radoslaw.kotkiewicz@gmail.com>
2026-02-03 22:32:54 +0100

    add system arch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOs Home Assistant configuration for Raspberry Pi 3 Model B";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     hardware.url = "github:nixos/nixos-hardware";
   };
@@ -10,7 +10,8 @@
   outputs = { self, nixpkgs, hardware, ... }@inputs: {
     nixosConfigurations = {
       "malina" = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
+
+        system = "aarch64-linux";
 
         modules = [
           ./configuration.nix
