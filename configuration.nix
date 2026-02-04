07463e21dc6cfd626e04975ecbb72f1a7c382fc0
Radosaw Kotkiewicz <radoslaw.kotkiewicz@gmail.com>
2026-02-04 18:42:34 +0100

    cleanup

--- configuration.nix
+++ configuration.nix
@@ -10,137 +10,43 @@
       ./hardware-configuration.nix
     ];
 
-  # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
   boot.loader.grub.enable = false;
-  # Enables the generation of /boot/extlinux/extlinux.conf
   boot.loader.generic-extlinux-compatible.enable = true;
 
   boot.kernelModules = [ "bcm2835-v4l2" ];
 
   hardware.enableRedistributableFirmware = true;
 
-  networking.hostName = "malina"; # Define your hostname.
-#  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "malina";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
   networking.useDHCP = false;
   networking.interfaces.eth0.useDHCP = true;
-#  networking.interfaces.wlan0.useDHCP = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "pl_PL.UTF-8";
   console = {
     font = "Lat2-Terminus16";
     keyMap = "pl";
   };
 
-  # Set your time zone.
   time.timeZone = "Europe/Warsaw";
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    wget
-    vim
     git
   ];
-  
-#  services.home-assistant = {
 
- #   enable = true;
-
-  #  port = 8123;
-
-   # config = {
-
-    #  homeassistant = {
-
-     #   name = "Home";
-
-      #  time_zone = "Europe/Warsaw";
-      #  latitude =  51.107883;
-     #   longitude = 17.038538;
-     #   elevation = 117;
-     #   unit_system = "metric";
-     #   temperature_unit = "C";
-     # };
-
-      # Enable the frontend
-
-      #frontend = {};
-     # http = {};
-
-   # };
-
- # };
-
-
-#  programs.git = {
-#    enable = true;
-#    userName  = "radekk";
-#    userEmail = "radoslaw.kotkiewicz@gmail.com";
-#  };
-
-
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  #   pinentryFlavor = "gnome3";
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 8080 8123 ];
   networking.firewall.allowedUDPPorts = [ 8080 ];
-  
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
-
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.nixos = {
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
   };
- # users.users.kodi = { 
- #  isNormalUser = true;
- # };
-  
+
+  swapDevices = [{ device = "/swapfile"; size = 1024; }];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -149,30 +55,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "20.09"; # Did you read the comment?
-
-  #hardware.enableRedistributableFirmware = true;
-
-  #fileSystems = {
-  #  "/" = {
-  #    device = "/dev/disk/by-label/NIXOS_SD";
-  #    fsType = "ext4";
-  #  };
-  #};
-
-  swapDevices = [{ device = "/swapfile"; size = 1024; }];
-  
-  
-#  services.xserver.enable = true;
-#  services.xserver.videoDrivers = [ "modesettings" ];
-
-#  services.xserver.desktopManager.kodi.enable = true;
-
-#  services.xserver.displayManager.autoLogin.enable = true;
-#  services.xserver.displayManager.autoLogin.user = "kodi";
-
-  # Define a user account
-#  users.extraUsers.kodi.isNormalUser = true;  
-#  services.cage.user = "kodi";
-#  services.cage.program = "${pkgs.kodi-wayland}/bin/kodi-standalone";
-#  services.cage.enable = true;
 }
